ab0c21470562449822bd1de02b8724a3859b577e
doomdex84 <doomdex84@gmail.com>
2025-04-23 15:52:10 +0900

    요구하는Dan 만들기

--- src/main/java/com/koreaIT/java/AM_jsp/PrintDanServlet.java
+++ src/main/java/com/koreaIT/java/AM_jsp/PrintDanServlet.java
@@ -16,17 +16,19 @@ public class PrintDanServlet extends HttpServlet {
 	
 		response.setContentType("text/html;charset:UTF-8");
 		
-		response.getWriter().append("==8단==<br>");
-//		
-//		response.getWriter().append(String.format("%d * %d = %d <br>",8,1,8));
-//		response.getWriter().append(String.format("%d * %d = %d <br>",8,2,16));
 		
-		int dan = 8;
+		String inputedDan = request.getParameter("dan");
+		
+		System.out.println(inputedDan);
+		
+		int dan = Integer.parseInt(inputedDan);
+		
+		response.getWriter().append(String.format("==%d단==<br>",dan));
 		
 		for (int i=1; i<=9; i++) {
 			response.getWriter().append(String.format("%d * %d = %d <br>",dan,i,dan*i));
 		}
+		
+		
 	}
-
-
 }
